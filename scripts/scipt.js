14d18b8b99cecf7a8af3f61f791fe6b6d4f18645
Matt <matt@bodwell.us>
2016-06-07 21:00:32 -0400

    finished basics

--- scripts/scipt.js
+++ scripts/scipt.js
@@ -1,21 +1,26 @@
 $( document ).ready(function() {
-  $( "p" ).text( "The DOM is now loaded and can be manipulated." );
-    
-    
-    $(".button").click(function(){
-        $(".button").remove();
-        for (var i=0;i<100;i++)
+  
+    var count = 0;
+    for (var i=0;i<15000;i++)
         {
+            
             $("#grid").append("<div class='block'></div>");
             
         
             
         }
+    $(".button").click(function(){
+        
+        $(".block").css("background-color","white");
+        $("h3").text("0");
+        count=0;
                         
         
     });
     $('#grid').on("mouseenter",".block",function(){
-       $(this).addClass("altered-block"); 
+       $(this).css("background-color","black");
+        count++;
+        $('h3').text(count);
     });
         
     
